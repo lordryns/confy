bf3af632384f8b061d735c09736c8618b2cc0742
David <rynstheoverlord@gmail.com>
2025-11-24 20:39:00 +0100

    Added set config path and updated path subcommand to take --set flag for setting new .config path

--- cmd/confy/main.go
+++ cmd/confy/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/charmbracelet/log"
@@ -13,9 +14,60 @@ func main() {
 	var cmd = cli.Command{
 		Name: "confy", 
 		Usage: "Sort and manage configs easily",
+		Commands: []*cli.Command{commandGetConfigPath(), commandSetConfig()},
 	}
 
 	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
+	}
+}
+
+
+func commandSetConfig() *cli.Command {
+	return &cli.Command{
+		Name: "set", 
+		Usage: "Replaces config with new one (and backs up the existing one)",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name: "no-backup",
+				Value: false,
+				Usage: "Prevent backup during set command",
+				
+			},
+		},
+		Action: func(ctx context.Context, c *cli.Command) error {
+			return nil
+		},
+	}
+} 
+
+func commandGetConfigPath() *cli.Command {
+	return &cli.Command{
+		Name: "path",
+		Usage: "See current config path",
+		Flags: []cli.Flag{
+            &cli.StringFlag{
+                Name:  "set",
+				Aliases: []string{"s"},
+                Usage: "Set new config path",
+            },
+		},
+		Action: func(ctx context.Context, c *cli.Command) error {
+			var res = c.String("set")
+
+			if  len(res) > 0 {
+				log.Infof("Setting new config path to: %v\n", res)
+				return nil
+			}
+
+			var s, err = os.UserConfigDir()
+			if err != nil {
+				log.Error(err)
+			}
+
+			fmt.Println(s)
+
+			return err
+		},
 	}
 }
